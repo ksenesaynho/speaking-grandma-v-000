72e0cf929919c0d0f727b1ade7cd848164eeb776
Kristina Senesaynho <ksenesaynho@gmail.com>
2018-01-30 19:38:53 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -1,7 +1,14 @@
- def speak_to_grandma(phrase)
-   if phrase == phrase.upcase
-  puts "HUH?! SPEAK UP, SONNY!"
-else
-    return "NO, NOT SINCE 1938!"
-   end
- end
+puts "Hey Sonny! It's your lovely Grandmother! How are yeah?"
+response = nil
+bye = 0
+while bye < 3
+  response = gets.chomp
+  if response == "BYE"
+    puts "Hmmm... I would prefer..."
+    bye = (bye+1)
+  elsif response == response.upcase
+    puts "NO! NOT SINCE " + (1930+rand(21)).to_s + "!"
+  else
+    puts "Huh?! I CAN'T HEAR YOU!"
+  end
+end
