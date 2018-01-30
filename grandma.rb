58cf01c286330983bc3a88b9fb5713525e4ec027
Kristina Senesaynho <ksenesaynho@gmail.com>
2018-01-30 19:39:44 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -1,8 +1,8 @@
 def speak_to_grandma(phrase)
-+  if phrase != phrase.upcase()
+  if phrase != phrase.upcase()
      return "HUH?! SPEAK UP, SONNY!"
- +  else
- +    return "NO, NOT SINCE 1938!"
- +  end
- +end
- +
+   else
+     return "NO, NOT SINCE 1938!"
+   end
+ end
+ 
