1460aa0e7e8d8acd31ea2ed45ed85c7ea4626607
Kristina Senesaynho <ksenesaynho@gmail.com>
2018-01-30 19:31:22 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -1,13 +1,20 @@
-# Write a speak_to_grandma method.
+def speak_to_grandma(sentence)
+  while sentence != "BYE"
+    if sentence == sentence.upcase
+      puts "NO, NOT SINCE #{rand(1930..1950)}!"
+   else 
+      puts "HUH?! SPEAK UP, SONNY!"
+    end
+  end
+end
 
-# Whatever you say to grandma, she should respond with
-# HUH?! SPEAK UP, SONNY!
-# unless you shout it (type in all capitals).
-
-# If you shout, she can hear you (or at least she thinks so) 
-# and yells back
-
-# NO, NOT SINCE 1938!
-
-# However if you say 'I LOVE YOU GRANDMA!', she should respond with
-# 'I LOVE YOU TOO PUMPKIN!'
+#triple bonus:
+def speak_to_grandma(sentence)
+  while sentence != "BYE BYE BYE"
+    if sentence == sentence.upcase
+      puts "NO, NOT SINCE #{rand(1930..1950)}!"
+   else 
+      puts "HUH?! SPEAK UP, SONNY!"
+    end
+  end
+end
