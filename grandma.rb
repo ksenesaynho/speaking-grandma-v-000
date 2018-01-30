4656abc5f55e5531d0b251e07c31a9b5b53c3926
Kristina Senesaynho <ksenesaynho@gmail.com>
2018-01-30 19:36:43 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -1,7 +1,7 @@
- def speak_to_grandma(sentence)
-   if sentence == sentence.upcase
-     puts "NO, NOT SINCE 1938!"
-   else 
-     puts "HUH?! SPEAK UP, SONNY!"
+ def speak_to_grandma(phrase)
+   if phrase == phrase.upcase
+  puts "HUH?! SPEAK UP, SONNY!"
+else
+    return "NO, NOT SINCE 1938!"
    end
  end
